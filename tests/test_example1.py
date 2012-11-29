64c135a9474531f79a3c6243e1f7f6b480bf1958
George Lewis <ghl227@nyu.edu>
2012-11-29 09:25:40 -0500

    Improved the testing makefile, including the make all command

--- tests/test_example1.py
+++ tests/test_example1.py
@@ -15,20 +15,22 @@ def pois(k, lam):
 
 class TestExample1(unittest.TestCase):
     
-    (d, s, b) = make_variables("d[1, 0, 15], s[1,0,15], b[1, 0, 15]")
-    chan = node("chan", pois, [d, N_exp])
-
-    (x0, mu0, sigma0) = make_variables("x0[.2,-5,5], mu0[0,-5,5], sigma0[1,0,3]")
-    mass0 = node("mass0", gauss, {'x':x0, 'mu':mu0, 'sigma':sigma0})
     
-    (x1, mu1, sigma1) = make_variables("x1[1.2,-5,5], mu1[1,-5,5], sigma1[2,0,3]")
-    mass1 = node("mass1", gauss, {'x':x1, 'mu':mu1, 'sigma':sigma1})
-    
-    print "x0 val: ", x0.val, " x1 val: ", x1.val
-    
-    print "mass0: ", mass0.getVal()
-    print "mass1: ", mass1.getVal()
-    
-    inv_mass = node("inv_mass", invariant_mass, {"a": mass0, "b":mass1})
-    
-    return inv_mass
+    def test_example(self):
+        (d, s, b) = make_variables("d[1, 0, 15], s[1,0,15], b[1, 0, 15]")
+        chan = node("chan", pois, [d, N_exp])
+        
+        (x0, mu0, sigma0) = make_variables("x0[.2,-5,5], mu0[0,-5,5], sigma0[1,0,3]")
+        mass0 = node("mass0", gauss, {'x':x0, 'mu':mu0, 'sigma':sigma0})
+        
+        (x1, mu1, sigma1) = make_variables("x1[1.2,-5,5], mu1[1,-5,5], sigma1[2,0,3]")
+        mass1 = node("mass1", gauss, {'x':x1, 'mu':mu1, 'sigma':sigma1})
+        
+        print "x0 val: ", x0.val, " x1 val: ", x1.val
+        
+        print "mass0: ", mass0.getVal()
+        print "mass1: ", mass1.getVal()
+        
+        inv_mass = node("inv_mass", invariant_mass, {"a": mass0, "b":mass1})
+        
+        return inv_mass
